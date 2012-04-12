df992919b89ef1b7ad5068fb63e77e4cd6e1ac8c
Sam Oluwalana <soluwalana@modeln.com>
2012-04-12 13:29:01 -0700

    Added Mysql connector test/example

---
+++ server/examples/mysql_example.js
@@ -0,0 +1,39 @@
+var mysql = require('db-mysql');
+var mysqlConfig = {
+	hostname : 'localhost',
+	user : 'docent',
+	password : 'Docent_2012',
+	database : 'docent_db'
+};
+
+var db = new mysql.Database(mysqlConfig);
+
+db.on('error', function(error){
+	console.log(error);
+});
+
+db.on('ready', function(server){
+	console.log('Connected to '+server.hostname+ ' ('+server.version+')');
+	console.log(server);
+});
+
+db.connect(function(error){
+	console.log("Connected");
+	for ( prop in this.query()){
+		console.log(prop);
+	}
+		
+	this.query('create table newTable (field1 integer not null primary key, field2 integer not null)').execute();
+	this.query('create table newTable1 (field3 integer not null primary key, field4 integer not null)').execute();
+	this.query('show tables').execute(function(err, rows, cols){
+		console.log(rows.length);
+		console.log(cols);
+	});
+	this.query('insert into newTable values (1, 2), (3, 4), (5, 6)').execute();
+	this.query('select * from newTable').execute(function(err, rows, cols){
+		console.log(rows);
+		console.log(cols);
+	});
+	this.query('drop table newTable').execute();
+	this.query('drop table newTable1').execute();
+});
--- server/tests/test_environment.js
+++ server/tests/test_environment.js
@@ -1,17 +1,159 @@
 var should = require('should');
 
-var mysql = require('db-mysql');
+var mysqlConfig = {
+	hostname : 'localhost',
+	user : 'docent',
+	password : 'Docent_2012',
+	database : 'docent_db'
+};
 
-var CassandraSystem = require('cassandra-client').System;
-var cassSys = new CassandraSystem('127.0.0.1:9160');
 
+describe('Mysql Setup Test', function(){
+	it('Access/Connect', function(done){
+		var mysql = require('db-mysql');
+		var db = new mysql.Database(mysqlConfig);
+		
+		db.should.be.ok;
+		db.on('error', function(error){
+			error.should.not.be.ok;
+		});
+		db.on('ready', function(server){
+			console.log('Connected to '+server.hostname+ ' ('+server.version+')');
+			console.log(server);
+		});
+		db.connect.should.be.ok;
+		
+		console.log()
+		db.connect(function(error){
+			should.not.exist(error);
+			console.log("Connected");
+			done();
+		});
+	});
 
-describe('Array', function(){
-	describe('#indexOf()', function(){
-		it('should return -1 when the value is not present', function(){
-			[1,2,3].indexOf(5).should.equal(-1);
-			[1,2,3].indexOf(0).should.equal(-1);
+	it('Create Table/Drop Table', function(done){
+		var mysql = require('db-mysql');
+		var db = new mysql.Database(mysqlConfig);
+		db.should.be.ok;
+		db.connect.should.be.ok;
+		db.on('error', function (error){
+			console.log(error);
+			error.should.not.be.ok;
+		});
+		
+		
+		db.connect(function(error){
+			should.not.exist(error);
+			var numTables = 0;
+			var expectedNumTables = 0;
+			var numberCallback = function(error, rows, cols){
+				should.not.exist(error);
+				numTables = rows.length;
+			};
+
+			var numberAssert = function(error, rows, cols){
+				should.not.exist(error);
+				rows.length.should.be.equal(expectedNumTables);
+			};
+
+			this.query('show tables').execute(numberCallback, { async : false});
+			expectedNumTables = numTables;
+			this.query('show tables').execute(numberAssert, { async : false});
+
+			var q1 = 'create table newTestTable0 (field1 integer primary key, field2 integer)';
+			var q2 = 'create table newTestTable1 (field1 integer primary key, field2 integer)';
+			this.query(q1).execute({ async : false });
+			this.query(q2).execute({ async : false });
+
+			expectedNumTables = numTables + 2;
+			this.query('show tables').execute(numberAssert, { async : false});
+
+			var q3 = 'drop table newTestTable0';
+			var q4 = 'drop table newTestTable1';
+
+			this.query(q3).execute({ async : false});
+			this.query(q4).execute({ async : false});
+
+			expectedNumTables = numTables;
+			
+			this.query('show tables').execute(numberAssert, { async : false});
+			done();
+			
+		});
+		
+	});
+
+	it('CRUD Test', function(done){
+		var mysql = require('db-mysql');
+		var db = new mysql.Database(mysqlConfig);
+		db.should.be.ok;
+		db.connect.should.be.ok;
+
+		db.on('error', function (error){
+			should.not.exist(error);
+		});
+				
+		db.connect(function(error){
+			should.not.exist(error);
+			var q1 = 'create table CRUDTestTable (f1 integer primary key, f2 boolean, f3 varchar(255))';
+			this.query(q1).execute(function(){}, { async : false });
+
+			var numRows;
+			var errCheck = function (err, rows, cols){
+				should.not.exist(err);
+			};
+			
+			var getNumRows = function(err, rows, cols){
+				should.not.exist(err);
+				numRows = rows.length ? rows.length : 0;
+			};
+
+			this.query().delete().from('CRUDTestTable').where("f1 != -1").execute(getNumRows, { async : false});
+
+			this.query().select('*').from('CRUDTestTable').execute(getNumRows, { async : false});
+			numRows.should.be.equal(0);
+			
+			this.query().insert('CRUDTestTable',
+								['f1', 'f2', 'f3'],
+								[[1, true, 'Test 1'],
+								 [2, false, 'Test 2'],
+								 [3, true, 'To Be changed']
+								]).execute(getNumRows, { async : false});
+
+			this.query().select('*').from('CRUDTestTable').execute(getNumRows, { async : false});
+			numRows.should.be.equal(3);
+
+			this.query().update('CRUDTestTable').
+				set({'f3' : 'Test 3'}).
+				where('f1 = 3').
+				execute(errCheck, { async : false});
+
+			this.query().select('*').from('CRUDTestTable').where('f1 = 3').execute(function(err, rows, cols){
+				should.not.exist(err);
+				rows.length.should.be.equal(1);
+				var res = rows[0];
+				res.f3.should.be.equal('Test 3');
+			}, {async : false});
+		
+			
+			this.query().delete().from('CRUDTestTable').execute(getNumRows, { async : false});
+			numRows.should.be.equal(0);
+
+			this.query('drop table CRUDTestTable').execute({async : false});
+
+			this.query('describe CRUDTestTable').execute(function (err, rows, cols){
+				err.should.be.ok;
+			}, {async : false});
+			
+			done();
 		});
 	});
 });
 
+/*
+describe('Cassandra Setup Test', function(){
+	it('Access Name Space', function(){
+		var CassandraSystem = require('cassandra-client').System;
+		var cassSys = new CassandraSystem('1.0.0.1:9160');
+	});
+});*/
